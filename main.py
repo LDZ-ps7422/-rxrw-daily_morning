f9cb86ccf8a154b7b7bbd21919f13c0f9f4686a5
LDZ-ps7422 <164450986+LDZ-ps7422@users.noreply.github.com>
2024-05-10 18:22:04 +0800

    Update main.py

--- main.py
+++ main.py
@@ -6,8 +6,7 @@ import requests
 import os
 import random
 
-today = datetime.now().date()
-#today = today_date.strftime("%Y-%m-%d")
+today = datetime.now()
 start_date = os.environ['START_DATE']
 city = os.environ['CITY']
 birthday = os.environ['BIRTHDAY']
